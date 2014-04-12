0ee06b319b038937dc6877dc4ee454204c54e3b3
teuteuguy <pub@lalatte.com>
2014-04-12 15:25:43 +0800

    Updated for click

--- js/services/servicesDefault.js
+++ js/services/servicesDefault.js
@@ -7,7 +7,7 @@
 // In this case it is a simple value service.
 var wookieServices = angular.module('wookiesApp.servicesDefault', []);
 
-wookieServices.value('version', '0.43');
+wookieServices.value('version', '0.44');
 
 wookieServices.value('utils', function() {
 
